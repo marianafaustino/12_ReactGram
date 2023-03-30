f7b2b86f66c52739a77f51bdac9b07868c11e0f0
Mariana Faustino <marifaustino@gmail.com>
2023-03-30 16:43:53 -0300

    Configurações de.env e rotas de teste

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,21 @@
+require("dotenv").config()
+
 const express = require("express")
 const path = require("path")
 const cors = require("cors")
 
-const port = 5000
+const port = process.env.PORT
 
 const app = express()
 
 // Configurações para receber as respostas em JSON e form data (por onde será possível enviar imagens)
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+// routes
+const router = require("./routes/Router.js")
+
+app.use(router)
 
 app.listen(port, ()=>{
     console.log(`App rodando na porta ${port}.`)
