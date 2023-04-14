bd9d69d9101b547db864e42c4d8b6f334f8bcec1
Mariana Faustino <marifaustino@gmail.com>
2023-04-14 17:18:26 -0300

    Resgatando foto por id

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -81,10 +81,27 @@ const insertPhoto = async(req,res)=>{
         return res.status(200).json(photos)
     }
 
+    // Resgatando fotos pelo id
+    const getPhotoById = async(req,res)=>{
+
+        const {id} = req.params
+
+        const photo = await Photo.findById(new mongoose.Types.ObjectId(id))
+
+        // Checando se a foto existe
+        if(!photo){
+            res.status(404).json({errors: ["Foto não encontrada."]})
+            return
+        }
+
+        res.status(200).json(photo)
+    }
+
 
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
-    getUserPhotos
+    getUserPhotos,
+    getPhotoById
 }
