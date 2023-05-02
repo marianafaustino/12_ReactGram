41cc3ba142f35791b5a950d8f0f91c2df9db197a
Mariana Faustino <marifaustino@gmail.com>
2023-05-02 15:33:32 -0300

    Criação do componente de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Cadastrar from './routes/Cadastrar';
+import Login from './routes/Login';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
-     <Cadastrar/>
+     <Login/>
     </div>
   );
 }
--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Box from '../components/Box'
+import Input from '../form/Input'
 
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+
   return (
-    <div>Login</div>
+    <Box
+    title='ReactGram'
+    subtitle='Faça o login para ver o que há de novo.'
+    msgRedirect='Não tem uma conta?'
+    width='50%'
+    height='50%'>
+
+      <form>
+
+        <Input
+          type='email'
+          id='email'
+          placeholder='Email'
+          onChange={setEmail}
+        />
+
+        <Input
+        type='password'
+        id='senha'
+        placeholder='Senha'
+        onChange={setSenha}
+        />
+
+      </form>
+    </Box>
+    
   )
 }
 
