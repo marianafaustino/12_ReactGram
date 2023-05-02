0d226afd292fc15d3d94f9bbc1f8a891d673a8bb
Mariana Faustino <marifaustino@gmail.com>
2023-05-02 19:56:08 -0300

    Criação do contexto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,26 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Cadastrar from './routes/Cadastrar';
 import Login from './routes/Login';
+import Feed from './routes/Feed';
+import { AuthProvider } from './context/AuthContext';
+import { useState } from 'react';
 
 function App() {
+
+  const [tokenUser, setTokenUser] = useState(undefined)
+  
   return (
     <div className="App">
-
+      <AuthProvider value={{tokenUser, setTokenUser}}>
       <BrowserRouter>
         <Navbar/>
         <Routes>
+          <Route path='/' element={<Feed/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Cadastrar/>}/>
         </Routes>
       </BrowserRouter>
-     
+      </AuthProvider>
     </div>
   );
 }
