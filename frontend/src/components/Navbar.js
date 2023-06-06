14b0a9a447ef9bfb7b5c8f5724473836da7452c7
Mariana Faustino <marifaustino@gmail.com>
2023-06-06 17:20:24 -0300

    CSS navbar e footer

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -12,20 +12,28 @@ const Navbar = () => {
   return (
     <nav id="nav">
         <Link to='/'>ReactGram</Link>
-        <form>
+        <form id='search-form'>
             <BsSearch/>
-            <input type="text" />
+            <input type="text" placeholder='Pesquisar' />
         </form>
         <ul id="nav-links">
+            <li>
             <NavLink to='/'>
                 <BsHouseDoorFill/>
             </NavLink>
+            </li>
+            
+            <li>
             <NavLink to='/login'>
                 Entrar
             </NavLink>
+            </li>
+            
+            <li>
             <NavLink to='/register'>
                 Cadastrar
             </NavLink>
+            </li>
         </ul>
     </nav>
   )
