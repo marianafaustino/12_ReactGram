9108b441059456dbc14b0cbb404db430d27ebbd6
Mariana Faustino <marifaustino@gmail.com>
2023-06-06 16:53:27 -0300

    Criação dos componentes footer e navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,15 +8,21 @@ import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 
+//components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
       </Routes>
+      <Footer/>
      </BrowserRouter>
     </div>
   );
