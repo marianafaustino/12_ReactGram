d14494baca2c94083d53b50aeb530caebc4ae30e
Mariana Faustino <marifaustino@gmail.com>
2023-06-07 15:16:32 -0300

    Criando states form de cadastro

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -8,20 +8,58 @@ import { useState, useEffect } from 'react'
 
 const Register = () => {
 
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
   const handleSubmit = (e)=>{
     e.preventDefault()
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user)
   }
+
   return (
     <div id='register'>
       <h2>ReactGram</h2>
       <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
 
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Nome' />
-        <input type="email" placeholder='Email' />
-        <input type="password" placeholder='Senha' />
-        <input type="password" placeholder='Confirme sua senha' />
-        <input type="submit" value="Cadastrar" />
+        <input 
+          type="text" 
+          placeholder='Nome' 
+          onChange={(e)=> setName(e.target.value)} 
+          value={name || ""} 
+        />
+        <input 
+          type="email" 
+          placeholder='Email' 
+          onChange={(e)=> setEmail(e.target.value)} 
+          value={email || ""} 
+        />
+        <input 
+          type="password" 
+          placeholder='Senha' 
+          onChange={(e)=> setPassword(e.target.value)} 
+          value={password || ""} 
+        />
+        <input 
+          type="password" 
+          placeholder='Confirme sua senha' 
+          onChange={(e)=> setConfirmPassword(e.target.value)} 
+          value={confirmPassword || ""} 
+        />
+        <input 
+          type="submit" 
+          value="Cadastrar" 
+        />
       </form>
 
       <p>Já tem conta? <Link to='/login'>Clique aqui</Link>.</p>
