b673d45e20c5d749aa1851d25117d83667052c48
Mariana Faustino <marifaustino@gmail.com>
2023-06-12 17:15:26 -0300

    verifica se o user está autenticado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 //routes
 import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 
+//hooks
+import { useAuth } from './hooks/useAuth';
+
 //pages
 import Home from './pages/Home/Home'
 import Login from './pages/Auth/Login'
@@ -13,15 +16,22 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
+
+  const {loading, auth} = useAuth()
+
+  if(loading){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
      <BrowserRouter>
       <Navbar/>
         <div className="container">
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
+          <Route path='/' element={auth ? <Home/> : <Navigate to="/login"/>}/>
+          <Route path='/login' element={!auth ? <Login/> : <Navigate to="/"/>}/>
+          <Route path='/register' element={!auth ? <Register/> : <Navigate to="/"/>}/>
         </Routes>
         </div>
       <Footer/>
