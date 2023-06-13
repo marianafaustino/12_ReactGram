eb10cc60c3d30308749326b94af7104c907075a1
Mariana Faustino <marifaustino@gmail.com>
2023-06-13 15:07:34 -0300

    slice do login

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -28,6 +28,20 @@ export const logout = createAsyncThunk("auth/logout", async ()=>{
     await authService.logout()
 })
 
+//sign in an user
+export const login = createAsyncThunk("auth/login",
+    async(user, thunkAPI)=>{
+        const data = await authService.login(user)
+
+        //check for errors
+        if(data.errors){
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    })
+
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -58,6 +72,21 @@ export const authSlice = createSlice({
             state.loading = false;
             state.sucess = true;
             state.error = null;
+            state.user = null
+        })
+        .addCase(login.pending, (state)=>{
+            state.loading = true;
+            state.error = false
+        })
+        .addCase(login.fulfilled, (state, action)=>{
+            state.loading = false;
+            state.sucess = true;
+            state.error = null;
+            state.user = action.payload
+        })
+        .addCase(login.rejected, (state, action)=>{
+            state.loading = false;
+            state.error = action.payload;
             state.user = null
         })
     }
