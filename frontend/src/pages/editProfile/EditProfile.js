b7fa3de67c1d09acc7232d3fc11ee67e85bf20e0
Mariana Faustino <marifaustino@gmail.com>
2023-06-13 17:24:15 -0300

    Exibindo prévia da imagem de perfil

--- frontend/src/pages/editProfile/EditProfile.js
+++ frontend/src/pages/editProfile/EditProfile.js
@@ -41,18 +41,37 @@ const EditProfile = () => {
     const handleSubmit = (e)=>{
         e.preventDefault()
     }
+
+    const handleFile = (e)=>{
+        //image preview
+        const image = e.target.files[0]
+
+        setPreviewImage(image)
+
+        //update image state
+        setProfileImage(image)
+    }
+
   return (
     <div id='edit-profile'>
         <h2>Edite seus dados</h2>
         <p className="subtitle">Adicione uma foto de perfil e conte mais sobre você...</p>
-        {/*preview da imagem */}
+        {(user.profileImage || previewImage) && (
+            <img className='profile-image'
+                src={
+                    previewImage ? URL.createObjectURL(previewImage) : `${uploads}/users/${user.profileImage}`
+                }
+                alt={user.name}
+            />
+
+        )}
 
         <form onSubmit={handleSubmit}>
             <input type="text" placeholder='Nome' onChange={(e)=> setName(e.target.value)} value={name || ""}/>
             <input type="email" placeholder='Email' disabled value={email || ""}/>
             <label>
                 <span>Imagem de perfil:</span>
-                <input type="file"/>
+                <input type="file" onChange={handleFile}/>
             </label>
             <label>
                 <span>Bio:</span>
