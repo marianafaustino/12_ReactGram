54c9e2cef47171472ff2e74b6182f60a51ef02bb
Mariana Faustino <marifaustino@gmail.com>
2023-06-14 16:48:04 -0300

    Slice dos dados do usuário

--- frontend/src/slices/userSlice.js
+++ frontend/src/slices/userSlice.js
@@ -36,6 +36,15 @@ export const updateProfile = createAsyncThunk(
     }
 )
 
+//get user details
+export const getUserDetails = createAsyncThunk(
+    "user/get",
+    async (id, thunkAPI)=>{
+        const data = await userService.getUserDetails(id)
+        return data
+    }
+)
+
 export const userSlice = createSlice({
     name: "user",
     initialState,
@@ -70,6 +79,16 @@ export const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
             state.user = {}
+        })
+        .addCase(getUserDetails.pending, (state)=>{
+            state.loading = true;
+            state.error = false
+        })
+        .addCase(getUserDetails.fulfilled, (state, action)=>{
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.user = action.payload
         })
     }
 })
