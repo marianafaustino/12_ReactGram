d631f227d227c84db2de07011d0abbec07143de0
Mariana Faustino <marifaustino@gmail.com>
2023-06-19 21:35:15 -0300

    service + slice de edição de fotos

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -45,10 +45,26 @@ const deletePhoto = async(id, token)=>{
     }
 }
 
+//update a photo
+const updatePhoto = async(data, id, token)=>{
+    const config = ("PUT", data, token)
+
+    try {
+        const res = await fetch(api + "/photos/" + id, config)
+                    .then((res)=> res.json())
+                    .catch((err)=> err)
+        return res
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const photoService = {
     publishPhoto,
     getUserPhotos,
-    deletePhoto
+    deletePhoto,
+    updatePhoto
 }
 
 export default photoService
--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -54,6 +54,22 @@ export const deletePhoto = createAsyncThunk(
     }
 )
 
+//update a photo
+export const updatePhoto = createAsyncThunk(
+    "photo/update",
+    async(photoData, thunkAPI)=>{
+        const token = thunkAPI.getState().auth.user.token
+        const data = await photoService.updatePhoto({title: photoData.title},photoData.id, token)
+
+         //check for errors
+         if(data.errors){
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+    }
+)
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -104,6 +120,28 @@ export const photoSlice = createSlice({
             state.message = action.payload.message
         })
         .addCase(deletePhoto.rejected, (state, action)=>{
+            state.loading = false;
+            state.error = action.payload;
+            state.photo = {}
+        })
+        .addCase(updatePhoto.pending, (state)=>{
+            state.loading = true;
+            state.error = null
+        })
+        .addCase(updatePhoto.fulfilled, (state, action)=>{
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photos.map((photo)=>{
+                if(photo._id === action.payload.photo._id){
+                    return photo.title = action.payload.photo.title 
+                }
+                return photo
+            })
+
+            state.message = action.payload.message
+        })
+        .addCase(updatePhoto.rejected, (state, action)=>{
             state.loading = false;
             state.error = action.payload;
             state.photo = {}
