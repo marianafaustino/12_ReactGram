cb5512471e7ef05d4047b2ad3c75dcb83a2a20de
Mariana Faustino <marifaustino@gmail.com>
2023-06-26 11:13:24 -0300

    Finalizando home page

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -16,7 +16,7 @@ import { getPhotos, like } from '../../slices/photoSlice'
 const Home = () => {
 
   const dispatch = useDispatch()
-  const resetMessage = useResetComponentMessage()
+  const resetMessage = useResetComponentMessage(dispatch)
   const {user} = useSelector((state)=> state.auth)
   const {photos, loading} = useSelector((state)=> state.photo)
 
@@ -36,7 +36,21 @@ const Home = () => {
   }
 
   return (
-    <div>Home</div>
+    <div id='home'>
+      {photos && photos.map((photo)=>(
+        <div key={photo._id}>
+          <PhotoItem photo={photo}/>
+          <LikeContainer photo={photo} user={user} handleLike={handleLike}/>
+          <Link className='btn' to={`/photos/${photo._id}`}>Ver mais</Link>
+        </div>
+      ))}
+
+      {photos && photos.length === 0 && (
+        <h2 className='no-photos'>Ainda não há fotos publicadas, 
+          <Link to={`/users/${user._id}`}>clique aqui</Link>.
+        </h2>
+      )}
+    </div>
   )
 }
 
